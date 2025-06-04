d5c1e9534a0ddd3587f559f03c8fd502a67efdf8
Zdravka Strahinova <zdravka.strahinova@primeholding.com>
2025-06-04 22:45:33 +0300

    Fix imports

--- src/features/tenants/hooks/useUpdateTenant.ts
+++ src/features/tenants/hooks/useUpdateTenant.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { Tenant } from '@/app/features/tenants/types';
 import { tenantsConstants } from '@/features/tenants/constants/tenants.constants';
 import { updateTenant } from '@/features/tenants/api/tenantsApi';
+import { Tenant } from '../interfaces/tenant.interface';
 
 export const useUpdateTenant = () => {
   const queryClient = useQueryClient();
