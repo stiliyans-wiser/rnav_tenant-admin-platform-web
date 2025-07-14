69204812eadd376ae418aa1c25bd9d475e9fba3c
Nikol Tsapreva <nikol.tsapreva@primeholding.com>
2025-07-14 17:30:56 +0300

    PAISE-140 Fix the base path

--- src/features/common/providers/ClientProviders.tsx
+++ src/features/common/providers/ClientProviders.tsx
@@ -21,7 +21,7 @@ const ProvidersContent = ({ children }: { children: React.ReactNode }) => {
 
 export const ClientProviders = ({ children }: ClientProvidersProps) => {
   return (
-    <SessionProvider refetchInterval={0} refetchOnWindowFocus={false} refetchWhenOffline={false}>
+    <SessionProvider refetchInterval={0} refetchOnWindowFocus={false} refetchWhenOffline={false} basePath="/next-api/auth">
       <ProvidersContent>{children}</ProvidersContent>
     </SessionProvider>
   );
