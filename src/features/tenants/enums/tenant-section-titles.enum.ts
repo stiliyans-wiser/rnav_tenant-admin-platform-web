3d20cedacd5a78a8054a037286fad3f687a57e28
Zdravka Strahinova <zdravka.strahinova@primeholding.com>
2025-09-03 19:42:22 +0300

    PAISE-318 Update enum value

--- src/features/tenants/enums/tenant-section-titles.enum.ts
+++ src/features/tenants/enums/tenant-section-titles.enum.ts
@@ -1,7 +1,7 @@
 export enum TenantSectionTitlesEnum {
   GENERAL_DETAILS = 'General Details',
   BRAND_AND_THEMING = 'Brand & Theming',
-  AI_SERVICES = 'AI Factory',
+  AI_SERVICES = 'AI Services',
   SSO = 'SSO',
   DOCUMENTS = 'Documents',
   DOCUMENT_GROUPS = 'Group Types',
